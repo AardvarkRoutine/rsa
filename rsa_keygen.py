980257e3163cfa3f6a61b80550b3a88f3252fe2b
paulcmdhack <52427190+paulcmdhack@users.noreply.github.com>
2024-06-16 21:17:04 +0200

    got rid of __pycache__ and added .gitignore

--- rsa_keygen.py
+++ rsa_keygen.py
@@ -1,22 +1,22 @@
 import random
 import math
 
-def sieve(limit):
-    is_prime = [True] * (limit + 1)
-    p = 2
-    while (p * p <= limit):
-        if (is_prime[p] == True):
-            for i in range(p * p, limit + 1, p):
-                is_prime[i] = False
-        p += 1
-    for p in range(256, limit + 1):
-        if is_prime[p] and len(str(p)) == 4:
-            yield p
-
 class prime_gen:
     def __init__(self, limit=10000):
-        self.primes = list(sieve(limit))
+        self.primes = list(self.sieve(limit))
         random.shuffle(self.primes)
+
+    def sieve(self, limit):
+        is_prime = [True] * (limit + 1)
+        p = 2
+        while (p * p <= limit):
+            if (is_prime[p] == True):
+                for i in range(p * p, limit + 1, p):
+                    is_prime[i] = False
+            p += 1
+        for p in range(256, limit + 1):
+            if is_prime[p] and len(str(p)) == 4:
+                yield p
 
     def get_prime(self):
         if not self.primes:
