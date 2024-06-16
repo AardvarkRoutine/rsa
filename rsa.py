e747bd28faec992a98b069277fe718aabd50c972
paulcmdhack <52427190+paulcmdhack@users.noreply.github.com>
2024-06-16 22:14:08 +0200

    made the program function both as module and run by user

--- rsa.py
+++ rsa.py
@@ -1,21 +1,17 @@
 from rsa_keygen import RSAKeyGenerator, prime_gen
 
-class RSAOperations:
-    @staticmethod
-    def ModPotenzieren(x, y, n):
-        return pow(x, y, n)
+def ModPotenzieren(x, y, n):
+    return pow(x, y, n)
 
-    @staticmethod
-    def verschluesseln(key, klartext):
-        e, n = key
-        geheimtext = RSAOperations.ModPotenzieren(klartext, e, n)
-        return geheimtext
+def verschluesseln(key, klartext):
+    e, n = key
+    geheimtext = ModPotenzieren(klartext, e, n)
+    return geheimtext
 
-    @staticmethod
-    def entschluesseln(key, geheimtext):
-        d, n = key
-        klartext = RSAOperations.ModPotenzieren(geheimtext, d, n)
-        return klartext
+def entschluesseln(key, geheimtext):
+    d, n = key
+    klartext = ModPotenzieren(geheimtext, d, n)
+    return klartext
 
 def main():
     prime_generator = prime_gen()
@@ -25,10 +21,28 @@ def main():
     public_key = rsa_keygen.get_public_key()
     private_key = rsa_keygen.get_private_key()
 
+    print("Do you want to encrypt or decrypt? (e/d)")
+    choice = input()
+    if choice == "e":
+        print("Enter the plaintext:")
+        plaintext = int(input())
+        ciphertext = verschluesseln(public_key, plaintext)
+        print(f"Ciphertext: {ciphertext}, public key: {public_key}, private key: {private_key}")
+    elif choice == "d":
+        print("Enter the ciphertext:")
+        ciphertext = int(input())
+        print("Enter the private key in the format 'd,n':")
+        private_key_input = input()
+        d, n = map(int, private_key_input.split(','))
+        private_key = (d, n)
+        
+        plaintext = entschluesseln(private_key, ciphertext)
+        print("Plaintext:", plaintext)
+    else:
+        print("Invalid choice. Exiting...")
+
 def is_module():
     pass # TODO: Implement this function
 
 if __name__ == '__main__':
     main()
-else:
-    is_module()
